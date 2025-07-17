6abddc2fadcc9ae53d02c6fecccdd4c802251bec
qSheker <aldikzhaks@gmail.com>
2025-07-17 20:26:25 +0500

    Added to library Hibernate Envers. Part 1 Working with it

--- src/main/java/com/qsheker/school/entities/Student.java
+++ src/main/java/com/qsheker/school/entities/Student.java
@@ -2,6 +2,8 @@ package com.qsheker.school.entities;
 
 import com.qsheker.school.listeners.AuditListener;
 import lombok.*;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.List;
 @Builder
 @EntityListeners(value = AuditListener.class)
 @ToString(exclude = {"group","courses"})
+@Audited
 public class Student{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,14 +32,17 @@ public class Student{
     @Column(nullable = false, unique = true)
     private String email;
 
+    @NotAudited
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
 
+    @NotAudited
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private List<Grade> grades = new ArrayList<>();
 
+    @NotAudited
     @ManyToMany(mappedBy = "students")
     @Builder.Default
     private List<Course> courses = new ArrayList<>();
